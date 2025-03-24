706ff56da56ddc2c868836bf193a21f99fae415f
kof99athena <kof99athena@gmail.com>
2025-03-24 22:45:49 +0900

    Chore: DI 초기 세팅

---
+++ app/src/main/java/com/lca/lcaandroid/LcaApllication.kt
@@ -0,0 +1,7 @@
+package com.lca.lcaandroid
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class LcaApllication : Application()
--- app/src/main/java/com/lca/lcaandroid/MainActivity.kt
+++ app/src/main/java/com/lca/lcaandroid/MainActivity.kt
@@ -12,7 +12,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.lca.lcaandroid.ui.theme.LcaAndroidTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
