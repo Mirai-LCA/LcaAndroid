edd7a7470367bd25f65032d06a2d1ed69e5ff190
kof99athena <kof99athena@gmail.com>
2025-03-24 23:25:39 +0900

    Feat: Coil을 통해 이미지 가져오기

--- app/src/main/java/com/lca/lcaandroid/screen/home/HomeScreen.kt
+++ app/src/main/java/com/lca/lcaandroid/screen/home/HomeScreen.kt
@@ -1,21 +1,40 @@
 package com.lca.lcaandroid.screen.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import coil3.request.ImageRequest
+import coil3.request.crossfade
 
 @Composable
 fun HomeScreen() {
-    Box(modifier = Modifier
-        .fillMaxSize(),
-        contentAlignment = Alignment.Center) {
-        Text(text = " Welcome Home Screen!",)
+    Box(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            AsyncImage(
+                model = ImageRequest.Builder(LocalContext.current)
+                    .data("https://cdn.pixabay.com/photo/2025/01/09/13/56/cat-9321685_1280.jpg")
+                    .crossfade(true)
+                    .build(),
+                contentDescription = "Unsplash Image",
+                modifier = Modifier.size(300.dp)
+            )
+            Text(text = "Welcome Home Screen!")
+        }
     }
 }
